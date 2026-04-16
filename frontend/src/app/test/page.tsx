33a5e2dfcef9de4084728b433a9eb58dd6535730
ekwawWilson <ee.wilson@outlook.com>
2026-04-16 15:18:10 +0000

    cleanbackend

--- frontend/src/app/test/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function TestPage() {
-  return (
-    <div style={{ padding: '50px', backgroundColor: 'red', color: 'white', fontSize: '24px' }}>
-      <h1>TEST PAGE - If you see this, basic rendering works!</h1>
-      <p>Background should be RED with WHITE text</p>
-    </div>
-  );
-}
